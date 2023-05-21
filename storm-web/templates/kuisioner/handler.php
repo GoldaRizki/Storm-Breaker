c30782f86a1ece0a5048f1fe655cf23e4b05a940
goldarizki <goldarizki@gmail.com>
2023-05-22 00:23:58 +0700

    update terakhir untuk log geolocation

--- storm-web/templates/kuisioner/handler.php
+++ storm-web/templates/kuisioner/handler.php
@@ -9,6 +9,15 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     if(isset($_POST['posisi'])){
         $data = $_POST['posisi'];
         file_put_contents("result.txt",$data);
+
+        try {
+            $file = fopen("posisi.txt", "w");
+            fwrite($file, $data . "\n");
+            fclose($file);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+       
     }
 
     if(isset($_POST['data_formulir'])){
